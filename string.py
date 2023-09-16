2405c3a4c559a2c127fba2e2613b22ee8ec29053
Mahesh131005 <mahesh131005@gmail.com>
2023-09-16 19:15:05 +0530

    hjhj

---
+++ string.py
@@ -0,0 +1,8 @@
+message='thish is thanish'
+print("\n",message)
+message="THISH THANISH"
+print("\n",message)
+message="it's my pen"
+print("\n",message)
+message='"He where are you.". I am in anantapur'
+print("\n",message)
