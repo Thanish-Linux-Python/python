58f78b8021ba9c0db0109ddc4a07094d3ec66e8e
Mahesh131005 <mahesh131005@gmail.com>
2023-09-16 19:02:19 +0530

    ghuhh

---
+++ initflocatchar.py
@@ -0,0 +1,6 @@
+a=3
+b=00 
+message="this is thanish"
+print("\n given value is in integer format:",a)
+print("\n Given value is in float format:",b)
+print("\n text message in char format:",message)
